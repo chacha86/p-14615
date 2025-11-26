40b75ad9a03a2118dbbf21b79946452f9f9da922
chacha86 <taejincha0619@gmail.com>
2025-11-26 20:31:21 +0900

    work

--- src/main/java/com/example/demo/domain/post/post/ApiV1PostController.java
+++ src/main/java/com/example/demo/domain/post/post/ApiV1PostController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class ApiV1PostController {
@@ -18,6 +20,14 @@ public class ApiV1PostController {
         postService.write(title, content);
 
         return "글 작성이 완료되었습니다.";
+    }
+
+    @GetMapping("/api/v1/posts")
+    @ResponseBody
+    public List<Post> list() {
+        List<Post> postList = postService.list();
+
+        return postList;
     }
 
 }
