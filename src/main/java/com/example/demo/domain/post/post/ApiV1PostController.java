c1d82a6308ba680a4a8274576a4bf20f2495a892
chacha86 <taejincha0619@gmail.com>
2025-11-28 20:51:55 +0900

    work

--- src/main/java/com/example/demo/domain/post/post/ApiV1PostController.java
+++ src/main/java/com/example/demo/domain/post/post/ApiV1PostController.java
@@ -24,8 +24,7 @@ public class ApiV1PostController {
     static class WriteReqBody {
         private String title;
         private String content;
-        private String username;
-        private String password;
+        private String apiKey;
     }
 
 
@@ -33,24 +32,19 @@ public class ApiV1PostController {
     @ResponseBody
     public RsData write(@RequestBody WriteReqBody writeReqBody) {
 
-        // 우리 회원이 맞는지 체크
-        Optional<Member> opMember = memberService.findByUsername(writeReqBody.username);
+        Optional<Member> opMember = memberService.findByApiKey(writeReqBody.apiKey);
 
         if (opMember.isEmpty()) {
-            throw new ServiceException("401", "없는 회원입니다.");
+            throw new ServiceException("401", "잘못된 API키입니다.");
         }
 
         Member member = opMember.get();
-
-        if (!member.getPassword().equals(writeReqBody.password)) {
-            throw new ServiceException("401", "비밀번호를 틀렸습니다.");
-        }
 
         postService.write(writeReqBody.title, writeReqBody.content);
 
         RsData rsData = new RsData();
         rsData.setResultCode("200");
-        rsData.setMessage(writeReqBody.username + "님의 글 작성이 완료되었습니다.");
+        rsData.setMessage(member.getUsername() + "님의 글 작성이 완료되었습니다.");
 
         return rsData;
     }
