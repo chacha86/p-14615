e608681ecdd733d6e550d27121b6248156bb5e66
chacha86 <taejincha0619@gmail.com>
2025-11-28 16:18:30 +0900

    work

--- src/main/java/com/example/demo/domain/post/post/ApiV1PostController.java
+++ src/main/java/com/example/demo/domain/post/post/ApiV1PostController.java
@@ -18,24 +18,42 @@ public class ApiV1PostController {
 
     @PostMapping("/api/v1/posts")
     @ResponseBody
-    public String write(String title, String content, String username, String password) {
+    public RsData write(String title, String content, String username, String password) {
 
         // 우리 회원이 맞는지 체크
         Optional<Member> opMember = memberService.findByUsername(username);
         if(opMember.isEmpty()) {
-            return "없는 회원입니다.";
+//            return "없는 회원입니다."; // 인증 실패 - 401
+
+            RsData rsData = new RsData();
+            rsData.setResultCode("401");
+            rsData.setMessage("없는 회원입니다.");
+
+            return rsData;
         }
 
         Member member = opMember.get();
 
         // 우리 회원이 맞다면 정말 그 회원이 맞는지 체크
         if(!member.getPassword().equals(password)) {
-            return "비밀번호가 틀렸습니다.";
+//            return "비밀번호가 틀렸습니다."; // 401
+
+            RsData rsData = new RsData();
+            rsData.setResultCode("401");
+            rsData.setMessage("비밀번호가 틀렸습니다.");
+
+            return rsData;
+
         }
 
         postService.write(title, content);
 
-        return username + "님의 글 작성이 완료되었습니다.";
+//        return username + "님의 글 작성이 완료되었습니다."; // 200
+        RsData rsData = new RsData();
+        rsData.setResultCode("200");
+        rsData.setMessage(username + "님의 글 작성이 완료되었습니다.");
+
+        return rsData;
     }
 
     @GetMapping("/api/v1/posts")
---
+++ src/main/java/com/example/demo/domain/post/post/RsData.java
@@ -0,0 +1,10 @@
+package com.example.demo.domain.post.post;
+
+import lombok.Setter;
+
+@Setter
+public class RsData {
+
+    private String resultCode;
+    private String message;
+}
